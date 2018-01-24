c3ce2e8f1f9198cf17f1f8b960153fe9bf49a98a
Maria Jose Solis <majosm96@gmail.com>
2018-01-24 10:31:52 -0600

    first commit

---
+++ client/index.js
@@ -0,0 +1,11 @@
+/*
+    ./client/index.js
+    which is the webpack entry file
+*/
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.js'
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+
